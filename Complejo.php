b187f0caf8dd11259d8bf0cab582af92d769dcc7
Yuliya Kobruseva <dafna_89@bk.ru>
2019-03-11 09:01:40 +0100

    He borrado los comentarios que crea NetBeans

--- Complejo.php
+++ Complejo.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of Complejo
- *
- * @author Yuli
- */
 class Complejo {
 
     //put your code here
